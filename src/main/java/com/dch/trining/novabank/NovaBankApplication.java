b85660cc34fd84da17cad4e8c7b98833cbfdf0f9
DCH <dariuz.chmiela@gmail.com>
2026-08-08 12:09:29 +0200

    Initial commit

---
+++ src/main/java/com/dch/trining/novabank/NovaBankApplication.java
@@ -0,0 +1,13 @@
+package com.dch.trining.novabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovaBankApplication.class, args);
+    }
+
+}
