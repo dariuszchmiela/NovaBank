6a060ee3e8310c46e317a498bc12a615ca2c4b6a
DCH <dariuz.chmiela@gmail.com>
2026-08-09 09:58:55 +0200

    Add TransferRequest

--- src/main/java/com/dch/trining/novabank/dto/TransferRequest.java
+++ src/main/java/com/dch/trining/novabank/dto/TransferRequest.java
@@ -1,11 +1,25 @@
 package com.dch.trining.novabank.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
 public record TransferRequest(
+
+        @NotBlank
         String sourceAccountId,
+
+        @NotBlank
         String targetAccountId,
+
+        @NotNull
+        @DecimalMin(value = "0.01")
         BigDecimal amount,
+
+        @NotBlank
         String currency
+
 ) {
 }
