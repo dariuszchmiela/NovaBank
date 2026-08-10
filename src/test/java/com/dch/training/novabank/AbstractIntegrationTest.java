4f4ebc1b512f2f30e85fa942b9762a884596753a
DCH <dariuz.chmiela@gmail.com>
2026-08-10 09:09:52 +0200

    Fix tests

--- src/test/java/com/dch/training/novabank/AbstractIntegrationTest.java
+++ src/test/java/com/dch/training/novabank/AbstractIntegrationTest.java
@@ -3,23 +3,23 @@ package com.dch.training.novabank;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.kafka.ConfluentKafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
-@Testcontainers
 public abstract class AbstractIntegrationTest {
 
     private static final String POSTGRES_IMAGE = "postgres:18-alpine";
-    @Container
     @ServiceConnection
     protected static final PostgreSQLContainer<?> POSTGRES_CONTAINER =
             new PostgreSQLContainer<>(DockerImageName.parse(POSTGRES_IMAGE));
     private static final String KAFKA_IMAGE = "confluentinc/cp-kafka:7.6.1";
-    @Container
     @ServiceConnection
     protected static final ConfluentKafkaContainer KAFKA_CONTAINER =
             new ConfluentKafkaContainer(DockerImageName.parse(KAFKA_IMAGE));
+
+    static {
+        POSTGRES_CONTAINER.start();
+        KAFKA_CONTAINER.start();
+    }
 }
