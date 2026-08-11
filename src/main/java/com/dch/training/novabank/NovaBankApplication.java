f80d4715b2e6c6774e520e440deb99b6ced37f48
DCH <dariuz.chmiela@gmail.com>
2026-08-11 09:00:50 +0200

    Fix tests

--- src/main/java/com/dch/training/novabank/NovaBankApplication.java
+++ src/main/java/com/dch/training/novabank/NovaBankApplication.java
@@ -3,9 +3,11 @@ package com.dch.training.novabank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableScheduling
 public class NovaBankApplication {
     public static void main(String[] args) {
         SpringApplication.run(NovaBankApplication.class, args);
